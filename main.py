8759c32cc17789ccac0681de6e348b6d9a9975e9
https-sam <osgo1115@icloud.com>
2022-11-25 14:38:20 -0700

    feat: file and directory support

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from cryptography.fernet import Fernet
 import subprocess
-import sys
-
+import argparse
 
 class Crypt():
   def __init__(self, key=None):
@@ -22,15 +21,22 @@ class Crypt():
     with open(keyname, 'wb') as filekey:
       filekey.write(key)
 
-  def encode(self, path):
-    out = subprocess.check_output(["ls", "-A1", path])
-    files = out.decode('UTF-8').split('\n')
-    files = [line for line in files if line.strip()]
+  def encode(self, path, isDir):
+    if not self.key:
+      raise "Activate first . . . . ."
+
+    files = []
+    if isDir:
+      out = subprocess.check_output(["ls", "-A1", path])
+      files.extend(out.decode('UTF-8').split('\n'))
+      files = [line for line in files if line.strip()]
+    else:
+      files.append(path)
 
     for fileName in files:
       # feeding the key
       fernet = Fernet(self.key)
-      filePath = "".join([path,fileName])
+      filePath = "".join([path,fileName]) if isDir else f"./{path}"
 
       with open(filePath, 'rb') as file:
         content = file.read()
@@ -41,17 +47,22 @@ class Crypt():
         encrypted_file.write(encrypted)
 
 
-  def decode(self, path):
+  def decode(self, path, isDir):
     if not self.key:
       raise "Activate first . . . . ."
-    out = subprocess.check_output(["ls", "-A1", path])
-    files = out.decode('UTF-8').split('\n')
-    files = [line for line in files if line.strip()]
+
+    files = []
+    if isDir:
+      out = subprocess.check_output(["ls", "-A1", path])
+      files.extend(out.decode('UTF-8').split('\n'))
+      files = [line for line in files if line.strip()]
+    else:
+      files.append(path)
 
     for fileName in files:
       # feeding the key
       fernet = Fernet(self.key)
-      filePath = "".join([path,fileName])
+      filePath = "".join([path,fileName]) if isDir else f"./{path}"
 
       with open(filePath, 'rb') as file:
         content = file.read()
@@ -64,16 +75,29 @@ class Crypt():
 
 
 if __name__ == '__main__':
-  if len(sys.argv) < 2:
-    raise "Please provide arguments"
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument("-a", "--action", help="Action to perform (encode / decode / keygen)")
+  parser.add_argument("-f", "--file", help="file to be processed")
+  parser.add_argument("-d", "--directory", help="directory to be processed")
+  args = parser.parse_args()
+
+  # checking args
+  if args.file and args.directory:
+    raise "Target must be either a file or a directory, but both are specified."
+  elif not args.file and not args.directory and args.action != "keygen":
+    raise "Target must be specified."
   
   cp = Crypt()
   cp.activate("secret.key")
 
-  if sys.argv[1] == "encode":
-    cp.encode("./data/") # path to the folder that contains files to be encrypted
-  elif sys.argv[1] == "decode":
-    cp.decode("./data/") # path to the folder that contains files to be encrypted
-  elif sys.argv[1] == "keygen":
+  path = args.file or args.directory
+  isDir = args.directory != None
+
+  if args.action == "encode":
+    cp.encode(path, isDir)
+  elif args.action == "decode":
+    cp.decode(path, isDir)
+  elif args.action == "keygen":
     cp.keygen('secret.key')
     
